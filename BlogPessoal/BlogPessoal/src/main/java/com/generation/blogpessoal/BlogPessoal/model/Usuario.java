829fc0681c03e96f20bfff787473de7898238a6d
Jessica Curti <jessica.m.curti@gmail.com>
2022-01-31 10:31:41 -0300

    @Jess-Curti [TASK: Logo Blog Pessoal]

--- BlogPessoal/BlogPessoal/src/main/java/com/generation/blogpessoal/BlogPessoal/model/Usuario.java
+++ BlogPessoal/BlogPessoal/src/main/java/com/generation/blogpessoal/BlogPessoal/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -28,6 +29,7 @@ public class Usuario {
 	private String nome;
 	
 	@NotBlank
+	@Email
 	private String usuario;
 	
 	@NotBlank
